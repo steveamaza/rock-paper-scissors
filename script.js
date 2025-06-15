1a5cb216ab8ad47cfeb0311da1700f37e4e3800b
King <steveamaza@gmail.com>
2025-06-15 15:01:21 +0100

    Add playRound function to include game logic and update scores

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    const choice = ["rock", "paper", "scissors"];
-    return choice[Math.floor(Math.random() * 3)];
+    const computerChoice = ["rock", "paper", "scissors"];
+    return computerChoice[Math.floor(Math.random() * 3)];
 }
 
 function getHumanChoice() {
@@ -9,3 +11,28 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice().toLowerCase();
+    if (humanChoice == computerChoice) {
+        console.log("The game ended as a draw!")
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock")
+        computerScore += 1
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper")
+        computerScore += 1
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Paper beats Rock")
+        computerScore += 1
+    }
+    else {
+        console.log("You win")
+        humanScore += 1;
+    }
+}
+
+playRound();
